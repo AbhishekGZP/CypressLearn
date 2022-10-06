34ce455f1e262681b567858df9d2d98df1aa4a61
AbhishekGZP <abhishek.tyagi@gamezop.co>
2022-10-06 17:29:09 +0530

    Changes

--- e2e/bidzclash.cy.js
+++ e2e/bidzclash.cy.js
@@ -4,6 +4,7 @@ describe('Launch and login', ()=>{
     cy.visit('https://www.quizzop.com/')
   });
 
+  
 it('Checking  That User land on the the onboarding page', ()=>{
     cy.contains('begin').should('be.visible')
 })
@@ -13,7 +14,7 @@ it('Answer First And Second onboarding question & Check that user Got the Lets S
   cy.get('.undefined > .px-10 > :nth-child(2)').click()
     cy.contains('Question 2').should('be.visible')
     cy.get('.undefined > .px-10 > :nth-child(3)').click()
-    cy.contains('Play more quizzes and win more coins').should('be.visible')
+    cy.contains('Play more quizzes and wins more coins').should('be.visible')
 })
 
 it('Click on the Lets Start button after the Onboarding Question ', ()=>{
